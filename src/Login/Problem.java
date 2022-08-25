f2ec6f05aad36bb30776fc93ab8fe003a7e5c01d
ylkhn <ylkhnozl@gmail.com>
2022-08-25 09:10:22 -0400

    problem class from feature 10

---
+++ src/Login/Problem.java
@@ -0,0 +1,17 @@
+package Login;
+
+public class Problem {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+
+        //this line is important
+
+    }
+
+
+}
